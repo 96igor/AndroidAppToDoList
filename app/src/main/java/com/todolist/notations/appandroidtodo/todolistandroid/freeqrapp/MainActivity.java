b63ca768fc21e716f31b25ac74048b42afcd8a70
Игорь Царёв <tsarov88@gmail.com>
2024-10-10 16:15:17 +0300

    Initial commit

---
+++ app/src/main/java/com/todolist/notations/appandroidtodo/todolistandroid/freeqrapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.todolist.notations.appandroidtodo.todolistandroid.freeqrapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
